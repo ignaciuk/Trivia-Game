32c54a867f321c3c7b613e84bb48fd90f6441579
Hobbes <43449480+ignaciuk@users.noreply.github.com>
2019-02-18 08:18:49 -0500

    changed time

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -211,7 +211,6 @@ $(document).ready(function() {
         setInterval(function () {
             minutes = parseInt(timer / 60, 10);
             seconds = parseInt(timer % 60, 10);
-            // minutes = minutes < 10 ? "0" + minutes : minutes;
             seconds = seconds < 10 ? "0" + seconds : seconds;
             $("#show-timer").text("Time remaining: " + minutes + ":" + seconds);
             if (--timer < 0) {
